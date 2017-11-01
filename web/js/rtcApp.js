cfb3b56d00c3f20a66e815d41a6a685154908074
Diego Bispo <dbispo@gmail.com>
2017-11-01 18:10:45 -0200

    Commit Alteração rtsapp

--- web/js/rtcApp.js
+++ web/js/rtcApp.js
@@ -1,82 +1,129 @@
 !(function(exports) {
-  'use strict';
+    'use strict';
 
-  var debug;
+    var debug;
 
-  var _views = {
-    '/room/': {
-      mainView: 'RoomController',
-      dependencies: [
-        'RoomController'
-      ]
-    },
-    '/': {
-      mainView: 'LandingController',
-      dependencies: [
-        'LandingController'
-      ]
+    var _views = {
+        '/room/': {
+            mainView: 'RoomController',
+            dependencies: [
+                'RoomController'
+            ]
+        },
+        '/': {
+            mainView: 'LandingController',
+            dependencies: [
+                'LandingController'
+            ]
+        }
+    };
+
+    function getView() {
+        var pathViews = Object.keys(_views);
+        var numViews = pathViews.length;
+        var path = exports.document.location.pathname;
+        for (var i = 0; i < numViews; i++) {
+            if (path.startsWith(pathViews[i]) &&
+                _views[pathViews[i]]
+                .dependencies
+                .every(function(dependency) {
+                    return !!exports[dependency];
+                })) {
+                return exports[_views[pathViews[i]].mainView];
+            }
+        }
+        return null;
     }
-  };
 
-  function getView() {
-    var pathViews = Object.keys(_views);
-    var numViews = pathViews.length;
-    var path = exports.document.location.pathname;
-    for (var i = 0; i < numViews; i++) {
-      if (path.startsWith(pathViews[i]) &&
-        _views[pathViews[i]]
-          .dependencies
-          .every(function(dependency) {
-            return !!exports[dependency];
-          })) {
-        return exports[_views[pathViews[i]].mainView];
-      }
+    function init() {
+        debug = new Utils.MultiLevelLogger('rtcApp.js', Utils.MultiLevelLogger.DEFAULT_LEVELS.all);
+        debug.log('Initializing app');
+        var view = getView();
+        if (view) {
+            view.init();
+        } else {
+            debug.error('Couldn\'t find a view for ' + exports.document.location.pathname);
+        }
     }
-    return null;
-  }
 
-  function init() {
-    debug = new Utils.MultiLevelLogger('rtcApp.js', Utils.MultiLevelLogger.DEFAULT_LEVELS.all);
-    debug.log('Initializing app');
-    var view = getView();
-    if (view) {
-      view.init();
-    } else {
-      debug.error('Couldn\'t find a view for ' + exports.document.location.pathname);
-    }
-  }
-
-  exports.RTCApp = {
-    init: init
-  };
+    exports.RTCApp = {
+        init: init
+    };
 }(this));
 
+//*************************************************
+
+/** Close tab or browser */
+$(window).on("beforeunload", function(e) {
+    desconectarUser($('#roomName').val());
+});
+
+function desconectarUser(usuario) {
+    if (usuario == "atendente") {
+        $.ajax({
+            //url: 'http://localhost/0disklibras/api.php/destroyroom/'+idusuario,
+            url: 'https://sistema.disklibras.com.br/api.php/destroyroom/' + usuario,
+            data: {
+                format: 'json'
+            },
+            error: function(data) {
+                console.log("erro exit");
+            },
+            dataType: 'jsonp',
+            success: function(data) {},
+            type: 'GET',
+            async: false
+        });
+    } else {
+        $.ajax({
+            //url: 'http://localhost/0disklibras/api.php/leavequeue/'+idusuario,
+            url: 'https://sistema.disklibras.com.br/api.php/leavequeue/' + usuario,
+            data: {
+                format: 'json'
+            },
+            error: function(data) {
+                console.log("erro exit");
+            },
+            dataType: 'jsonp',
+            success: function(data) {},
+            type: 'GET',
+            async: false
+        });
+    }
+}
+
+
+
+
+
+
+//*************************************************
 
 this.addEventListener('load', function startApp() {
-  // Note that since the server forbids loading the content on an iframe this should not execute.
-  // But it doesn't hurt either
-  if (window.top !== window.self && !window.iframing_allowed) {
-    // If we're being loaded inside an iframe just hijack the top level window and go back to
-    // the index page.
-    window.top.document.location = '/index.html';
-  } else {
-    // And setting this on an else because the re-location might fail in some cases
-    document.body.classList.remove('forbidden');
-    // Check that everything was loaded correctly, or just use LazyLoader here...
-    LazyLoader.load([
-      '/js/libs/browser_utils.js',
-      '/shared/js/utils.js',
-      '/js/helpers/requests.js',
-      '/js/roomController.js',
-      '/js/landingController.js'
-    ]).then(function() {
-      RTCApp.init();
-    });
-  }
+    // Note that since the server forbids loading the content on an iframe this should not execute.
+    // But it doesn't hurt either
+    if (window.top !== window.self && !window.iframing_allowed) {
+        // If we're being loaded inside an iframe just hijack the top level window and go back to
+        // the index page.
+        window.top.document.location = '/index.html';
+    } else {
+        // And setting this on an else because the re-location might fail in some cases
+        document.body.classList.remove('forbidden');
+        // Check that everything was loaded correctly, or just use LazyLoader here...
+        LazyLoader.load([
+            '/js/libs/browser_utils.js',
+            '/shared/js/utils.js',
+            '/js/helpers/requests.js',
+            '/js/roomController.js',
+            '/js/landingController.js'
+        ]).then(function() {
+            RTCApp.init();
+        });
+    }
 
-// Allow only https on production
-  if (document.location.protocol === 'http:' &&
-    document.location.hostname.indexOf('.tokbox.com') > 0) {
-    document.location.href = document.location.href.replace(new RegExp('^http:'), 'https:');
-  }
+    // Allow only https on production
+    if (document.location.protocol === 'http:' &&
+        document.location.hostname.indexOf('.tokbox.com') > 0) {
+        document.location.href = document.location.href.replace(new RegExp('^http:'), 'https:');
+    }
 });
